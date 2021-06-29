2d86b9aea5798c9b021626eac592089d44cb6e94
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-06-29 19:47:27 -0300

    iniciando projeto

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Signin() {
+    return (
+        <View style={styles.container}>
+            <Text>Gameplay</Text>
+        </View>
+    );
+}
