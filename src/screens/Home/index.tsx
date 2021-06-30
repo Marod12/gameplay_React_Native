12d9acde6b24ac1d16d19de9205a08ceb1d47e0c
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-06-30 18:23:47 -0300

    ButtonAdd

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
+import { ButtonAdd } from '../../components/ButtonAdd';
 
 import { styles } from './styles';
 import { Profile } from '../../components/Profile';
 
 export function Home() {
     return (
-        <View 
-            style={styles.container}
-        >
-            <View 
-                style={styles.header}
-            >
+        <View style={styles.container}>
+            
+            <View style={styles.header}>
                 <Profile />
+                <ButtonAdd />
             </View>
 
             
