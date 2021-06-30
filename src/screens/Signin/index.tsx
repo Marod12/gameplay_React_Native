f5d27c55985872554858265b8b3e8b3bd9aea4bc
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-06-29 22:24:09 -0300

    Configs adicionais && splash / screns - Signin

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StatusBar } from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
+import illustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function Signin() {
     return (
         <View style={styles.container}>
-            <Text>Gameplay</Text>
+            <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image 
+                source={illustrationImg} 
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {`\n`}
+                    e organize suas {`\n`}
+                    jogatinas {`\n`}
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon 
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                />
+            </View>
         </View>
     );
 }
