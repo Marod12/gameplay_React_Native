f7a49b28d810b1d96b4ee70957d329f653779fab
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-07-02 17:19:41 -0300

    redirecionar para o servidor

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
 
 import { ImageBackground, Text, View, FlatList, Alert, Share, Platform } from 'react-native';
 
@@ -60,6 +61,10 @@ export function AppointmentDetails(){
         });
     }
 
+    function handleOpenGuild() {
+        Linking.openURL(widget.instant_invite);
+    }
+
     useEffect(() => {
         fetchGuildWidget();
     }, []);
@@ -115,7 +120,10 @@ export function AppointmentDetails(){
                     </>
             }        
             <View style={styles.footer}>
-                <ButtonIcon title="Entrar na partida" />
+                <ButtonIcon 
+                    title="Entrar na partida"
+                    onPress={handleOpenGuild}
+                />
             </View>
         </Background>
      );
