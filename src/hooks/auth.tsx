4274f60ac877304eb0e9dde938afa7d618d7fe84
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-07-02 13:06:47 -0300

    persistir os dados do usuário

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const { SCOPE } = process.env;
 const { CLIENT_ID } = process.env;
@@ -8,6 +9,7 @@ const { REDIRECT_URI } = process.env;
 const { RESPONSE_TYPE } = process.env;
 
 import { api } from '../services/api';
+import { COLLECTION_USERS } from '../configs/database';
 
 type User = {
     id: string;
@@ -61,11 +63,14 @@ function AuthProvider({ children }: AuthProviderProps) {
                 : `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`        
             ;
         
-            setUser({
+            const userData = {
                 ...userInfo.data,
                 firstName,
                 token: params.access_token
-            });
+            };
+
+            await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+            setUser(userData);
         }
         } catch {
             throw new Error('Não foi possível autenticar');
@@ -73,6 +78,21 @@ function AuthProvider({ children }: AuthProviderProps) {
             setLoading(false);
         }
     }
+
+    async function loadUserStorageData() {
+        const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+    
+        if (storage) {
+            const userLogged = JSON.parse(storage) as User;
+            api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+        
+            setUser(userLogged);
+        }
+    }
+    
+    useEffect(() => {
+        loadUserStorageData();
+    }, []);
 
     return (
         <AuthContext.Provider value={{
