688f69cd6745bf78dc9dda26b34b173f421d0fe3
Matheus Rodrigues <mathews-rodrigues@hotmail.com>
2021-07-02 16:52:33 -0300

    listar jogadores

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,45 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
 
-import { ImageBackground, Text, View, FlatList } from 'react-native';
+import { ImageBackground, Text, View, FlatList, Alert } from 'react-native';
 
 import BannerImg from '../../assets/banner.png';
 
 import { styles } from './styles';
 import { theme } from '../../global/theme';
+import { api } from '../../services/api';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
 import { ListHeader } from '../../components/ListHeader';
-import { Member } from '../../components/Member';
+import { Member, MemberProps } from '../../components/Member';
 import { ListDivider } from '../../components/ListDivider';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { AppointmentProps } from '../../components/Appointment';
+import { Load } from '../../components/Load';
 
 type Params = {
     guildSelected: AppointmentProps
 }
 
+type GuildWidget = {
+    id: string;
+    name: string;
+    instant_invite: string;
+    members: MemberProps[];
+}
+
 export function AppointmentDetails(){
+    const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+    const [loading, setLoading] = useState(true);
+
     const route = useRoute();
     const { guildSelected } = route.params as Params;
 
-    const members = [
-        {
-          id: '1',
-          username: 'Marod',
-          avatar_url: 'https://github.com/Marod12.png',
-          status: 'online'
-        },
-        {
-          id: '2',
-          username: 'Marod',
-          avatar_url: 'https://github.com/Marod12.png',
-          status: 'offline'
+    async function fetchGuildWidget() {
+        try {
+            const response = await api.get(`/guilds/${guildSelected.guild.id}/widget.json`);
+            setWidget(response.data);
+        } catch {
+            Alert.alert('Verifique as configurações do servidor. Será que o Widget está habilitado?');
+        } finally {
+            setLoading(false);
         }
-    ]
+    }
+
+    useEffect(() => {
+        fetchGuildWidget();
+    }, []);
     
     return (
         <Background>
@@ -70,22 +82,26 @@ export function AppointmentDetails(){
                 </Text>
                 </View>
             </ImageBackground>
+            
+            {
+                loading ? <Load /> :
+                    <>
+                        <ListHeader 
+                            title="Jogadores"
+                            subtitle={`Total ${widget.members.length ? widget.members.length : 0}`}
+                        />
 
-            <ListHeader 
-                title="Jogadores"
-                subtitle="Total 2"
-            />
-
-            <FlatList 
-                data={members}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                <Member data={item} />
-                )}
-                ItemSeparatorComponent={() => <ListDivider isCentered />}
-                style={styles.members}
-            />
-
+                        <FlatList 
+                            data={widget.members}
+                            keyExtractor={item => item.id}
+                            renderItem={({ item }) => (
+                            <Member data={item} />
+                            )}
+                            ItemSeparatorComponent={() => <ListDivider isCentered />}
+                            style={styles.members}
+                        />
+                    </>
+            }        
             <View style={styles.footer}>
                 <ButtonIcon title="Entrar na partida" />
             </View>
